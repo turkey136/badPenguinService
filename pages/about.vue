2776d44713856b1c5ec0c1e47721725bcd8ae244
turkey126 <isamu.takimoto.136@gmail.com>
2020-08-08 21:57:17 +0900

    Add about page

--- pages/about.vue
+++ pages/about.vue
@@ -2,9 +2,28 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="12">
-
-        </v-col>
+        <div>
+          フリーランスエンジニアが個人的に運営しているサイトです。
+          名前の通り、お金儲け優先の悪徳サイトですので、内容に責任を持てないものがあります。
+          記載の内容はそのまま信じないでご自分で再度確認をお願いします。
+        </div>
+        <br>
+        <div>
+          <h3>サイト運営者情報</h3>
+          <div>
+            氏名： 瀧元 勇（たきもと　いさむ）
+          </div>
+          <div>
+            住所： 日本
+          </div>
+          <div>
+            email： isamu.takimoto.136@gmail.com
+          </div>
+          <div>
+            GitHub： https://github.com/turkey136/badPenguinService
+          </div>
+        </div>
+      </v-col>
     </v-row>
   </v-container>
 </template>
-        
